e4aab309147076d9acf911ba51a49fd644735435
nickjuanx <nicooviedo901@gmail.com>
2022-11-07 18:26:32 -0300

    x

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,26 @@
+
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { modalController } from '@ionic/core';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'] 
 })
 export class Tab1Page {
 
   constructor() {}
 
-}
+  
 
+    isModalOpen = false;
+  
+    setOpen(isOpen: boolean) {
+      this.isModalOpen = isOpen;
+    }
+  
+  }
 
 
